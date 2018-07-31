bb968ec4f16141bc4016af5372e9fc787c17029e
DESKTOP-51SS5GR\fsingletonthorn <fsingletonthorn@gmail.com>
2018-08-01 07:36:09 +1000

    Adding a R project file and filling in missing data with the data from frequency tables - all freq tables bounds have been added and the numbers all double checked

--- R files (working, figures, general working doc)/Estimating means and SDs from freq tables.R
+++ R files (working, figures, general working doc)/Estimating means and SDs from freq tables.R
@@ -5,6 +5,12 @@ read.excel <- function(header=TRUE,...) {
   read.table("clipboard",sep="\t",header=header,...)
 }
 
+# alternative method
+# install.packages('binsmooth')
+library(binsmooth)
+# install.packages('bda')
+library(bda)
+
 # articles to check: 
 dat$id[which(is.na(dat$varMedium) & is.na(dat$IQRMedium))]
 # [1]  96  99  73  92  59  62  42  89 
@@ -18,27 +24,34 @@ dat$id[which(is.na(dat$varMedium) & is.na(dat$IQRMedium))]
 #  113 (no freq tables),121, 115, 123 (not avaliable), 119
 
 # cASHEN, gEIGER 2004 # only small avaliable 
-powers <- c(.99, 0.965, .92, .87, .82, .745, .645, .545, .445, .345, .245, .145, 0.07)
+lb <- c(.99, .95, .9, .85, .8, .7, .6, .5, .4, .3, .2, .1, .05)
+ub <-  c(1, .98, .94, .89, .84, .79, .69, .59, .49, .39, .29, .19, .09)
+powers <- rowMeans(data.frame(lb, ub))
 ns <- c(12, 1, 1, 1, 0, 0, 1, 1, 2, 10, 10, 29, 9)
 
 # Mazen  Hemmasi & Lewis 1987
-# Table 2 # paste(as.character(MAZ$small), collapse=", ")
-powers <-  c(0.990, 0.965, 0.920, 0.845, 0.745, 0.645, 0.545, 0.445, 0.345, 0.245, 0.095, 0.065)
+# Table 2 
+lb <- c(.99, .95, .9, .80, .7, .6, .5, .4, .3, .2, .1, .04)
+ub <-  c(1, .98, .94, .89, .79, .69, .59, .49, .39, .29, .19, .09)
+powers <-  rowMeans(data.frame(lb, ub))
 small <- c(0, 0, 2, 1, 1, 1, 0, 2, 2, 5, 16, 14)
 medium <- c(6, 2, 2, 3, 4, 4, 6, 4, 7, 3, 3, 0)
 large <- c(12, 6, 5, 6, 7, 3, 2, 1, 0, 1, 1, 0) 
 
 ns<- large # etc. 
 
-# Mone, M. A., Mueller, G. C. and Mauland, W. 1996
-
-powers <- c(0.995, 0.965, 0.92, 0.845, 0.745, 0.645, 0.545, 0.445, 0.345, 0.245, 0.145, 0.07)
+# Mone, M. A., Mueller, G. C. and Mauland, W. 1996, table 1
+lb <- c(.99, .95, .9, .80, .7, .6, .5, .4, .3, .2, .1, .05)
+ub <-  c(1, .98, .94, .89, .79, .69, .59, .49, .39, .29, .19, .09)
+powers <-  rowMeans(data.frame(lb, ub))
 small <- c(9, 3, 1, 5, 1, 6, 7, 9, 15, 43, 75, 36)
 medium <- c(49, 15, 21, 24, 20, 19, 21, 18, 8, 11, 3, 1)
 large <- c(124, 22, 17, 21, 8, 10, 4, 1, 2, 0, 1, 0)
 
 # Haase 1974 # table 1
-powers <- c(0.995, 0.965, 0.92, 0.845, 0.745, 0.645, 0.545, 0.445, 0.345, 0.245, 0.145, 0.07) 
+lb <- c(.99, .95, .9, .80, .7, .6, .5, .4, .3, .2, .1, .05)
+ub <-  c(1, .98, .94, .89, .79, .69, .59, .49, .39, .29, .19, .09)
+powers <-  rowMeans(data.frame(lb, ub))
 small <- c(2, 1, 1, 1, 1, 2, 6, 1, 1, 6, 16, 30) 
 medium <- c(4, 8, 5, 4, 3, 7, 7, 5, 5, 12, 12, 0) 
 large <- c(13, 0, 0, 4, 4, 3, 0, 5, 9, 2, 0, 0)
@@ -46,31 +59,43 @@ large <- c(13, 0, 0, 4, 4, 3, 0, 5, 9, 2, 0, 0)
 
 # Woolley, Thomas W.	A comprehensive power-analytic investigation of research in medical education 
 # table 2
-powers <- c(0.995, 0.965, 0.92, 0.845, 0.745, 0.645, 0.545, 0.445, 0.345, 0.245, 0.145, 0.07) 
+lb <- c(.99, .95, .9, .80, .7, .6, .5, .4, .3, .2, .1, .0)
+ub <-  c(1, .98, .94, .89, .79, .69, .59, .49, .39, .29, .19, .09)
+powers <-  rowMeans(data.frame(lb, ub))
 small <- c(1, 2, 0, 1, 0, 2, 3, 5, 7, 13, 47, 19) 
 medium <- c(17, 7, 9, 11, 7, 11, 13, 8, 8, 6, 3, 0)
 large <- c(50, 11, 8, 14, 6, 2, 5, 2, 1, 1, 0, 0)
  
 ## Borkowski, Susan C. Mary Jeanne Welsh and Zhang, Qinke	An Analysis of Statistical Power in Behavioral Accounting Research. Table 3
-powers <- c(0.97, 0.92, 0.845, 0.745, 0.645, 0.545, 0.445, 0.345, 0.245, 0.145, 0.045) 
+lb <- c(.95, .9, .8, .7, .6, .5, .4, .3, .2, .1, .0)
+ub <-  c(.99, .94, .89, .79, .69, .59, .49, .39, .29, .19, .09)
+powers <-  rowMeans(data.frame(lb, ub))
 small <- c(0, 0, 1, 2, 1, 0, 4, 5, 20, 50, 13) 
 medium <- c(10, 11, 13, 8, 12, 16, 12, 9, 5, 0, 0) 
 large <- c(51, 12, 14, 11, 1, 6, 0, 1, 0, 0, 0)
 
 # Woolley, Thomas W. and Dawson, George O.	A Follow-up Power Analysis of the Statistical Tests Used in the Journal of Research in Science Teaching. Table III
-powers <- c(0.995, 0.965, 0.92, 0.845, 0.745, 0.645, 0.545, 0.445, 0.345, 0.245, 0.145, 0.07) 
+lb <- c(.99, .95, .9, .80, .7, .6, .5, .4, .3, .2, .1, .0)
+ub <-  c(1, .98, .94, .89, .79, .69, .59, .49, .39, .29, .19, .09)
+powers <-  rowMeans(data.frame(lb, ub))
 small <- c(1, 3, 2, 2, 2, 5, 8, 9, 8, 29, 79, 44) 
 medium <- c(29, 8, 8, 22, 21, 13, 23, 24, 18, 13, 11, 2) 
 large <- c(65, 30, 21, 26, 15, 14, 7, 5, 4, 4, 1, 0)
 
-# Overland, C. T.	Statistical Power in the Journal of Research in Music Education (2000-2010): A Retrospective Power Analysis	Bulletin of the Council for Research in Music Education	2014	
-powers <- c(0.965, 0.92, 0.845, 0.745, 0.645, 0.545, 0.445, 0.345, 0.245, 0.145, 0.045) 
+# Overland, C. T.	Statistical Power in the Journal of Research in Music Education (2000-2010): A Retrospective Power Analysis	Bulletin of the Council for Research in Music Education	2014
+# table 2 
+lb <- c(.95, .9, .80, .7, .6, .5, .4, .3, .2, .1, .00)
+ub <-  c(.98, .94, .89, .79, .69, .59, .49, .39, .29, .19, .09)
+powers <-  rowMeans(data.frame(lb, ub))
 small <- c(4, 0, 2, 0, 4, 4, 6, 16, 17, 37, 35) 
 medium <- c(29, 3, 14, 16, 8, 12, 11, 9, 8, 11, 3) 
 large <- c(70, 9, 11, 12, 5, 4, 3, 2, 2, 5, 2)
 
 # Mazen, A. M. M., Hemmasi, M. and Lewis, M. F	Assessment of statistical power in contemporary strategy research
-powers <- c(0.995, 0.965, 0.92, 0.845, 0.745, 0.645, 0.545, 0.445, 0.345, 0.245, 0.145, 0.07) 
+# table 2
+lb <- c(.99, .95, .9, .80, .7, .6, .5, .4, .3, .2, .1, .04)
+ub <-  c(1, .98, .94, .89, .79, .69, .59, .49, .39, .29, .19, .09)
+powers <-  rowMeans(data.frame(lb, ub))
 small <- c(0, 0, 2, 1, 1, 1, 0, 2, 2, 5, 16, 14) 
 medium <- c(6, 2, 2, 3, 4, 4, 6, 4, 7, 3, 3, 0)
 large <- c(12, 6, 5, 6, 7, 3, 2, 1, 0, 1, 1, 0)
@@ -78,14 +103,18 @@ large <- c(12, 6, 5, 6, 7, 3, 2, 1, 0, 1, 1, 0)
 
 
 # used this to print the above, after preprocessing in excel
-temp<-read.excel(header = T)
-paste0(c("powers <- c(", paste(as.character(temp$Power), collapse=", "), ") \n small <- c(",
-        paste(as.character(temp$small), collapse=", "),  ") \n medium <- c(",
-        paste(as.character(temp$medium), collapse=", "),  ") \n large <- c(",
-        paste(as.character(temp$large), collapse=", "), ")"), collapse = "")
+# temp<-read.excel(header = T)
+# paste0(c("powers <- c(", paste(as.character(temp$Power), collapse=", "), ") \n small <- c(",
+#        paste(as.character(temp$small), collapse=", "),  ") \n medium <- c(",
+#        paste(as.character(temp$medium), collapse=", "),  ") \n large <- c(",
+#        paste(as.character(temp$large), collapse=", "), ")"), collapse = "")
+# 
+
+# paste(as.character(temp), collapse=", ")
+
 
 # all estiamted using: 
-ns<- medium # etc. 
+ns<- small # etc. 
 
 n <-sum(ns)
 estMean <- sum(powers * ns) / n
@@ -94,4 +123,26 @@ estSD <- sqrt(estVar)
 estSD
 
 
-paste(as.character(temp), collapse=", ")
+ub <-  rev(c(1, .98, .94, .89, .79, .69, .59, .49, .39, .29, .19, .09))
+medium <- rev(c(6, 2, 2, 3, 4, 4, 6, 4, 7, 3, 3, 0))
+
+
+
+
+fitted <- splinebins(bEdges =  (ub), bCounts = (medium), monoMethod = 'monoH.FC')
+
+bde(x = medium, counts = medium, breaks = ub, lbound = .05)
+
+plot(fitted$splinePDF(seq(0,1,.01)), x = (seq(0,1,.01)), type = 'l')
+plot(ub, medium, type = 'h')
+lines(fitted$splinePDF(seq(0,1,.01)), x = (seq(0,1,.01)))
+
+temp<-binning(counts=rev(medium)[-1], breaks= rev(ub))
+plot(fit.GB(temp, 0,1))
+plot(temp)
+
+
+
+
+
+
